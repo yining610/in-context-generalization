85b87f05645d2b899b22f3d57df39a94404ee6b5
yining610 <kinglu20000216@gmail.com>
2023-08-14 14:26:53 -0400

    update

--- evaluate.py
+++ evaluate.py
@@ -34,8 +34,8 @@ def compute_metric(result_path: str, model_path: str, data_path: str, data_name:
 
         with open(os.path.join(data_path, folder, f"{data_name}.jsonl"), "r") as f:
             data = f.readlines()[0]
-        data = json.loads(data)
-        demonstrations = data['prompt'].split("### Demonstration:")[1].split("### Input:")[0].strip()
+        demonstrations = json.loads(data)['prompt']
+        demonstrations = demonstrations.split("### Demonstration:")[1].split("### Input:")[0].strip()
         demonstrations_ids = tokenizer(demonstrations, return_tensors="pt")
         results["tokens"].append(demonstrations_ids["input_ids"].shape[1])
 
@@ -44,10 +44,11 @@ def compute_metric(result_path: str, model_path: str, data_path: str, data_name:
 
     return df
 
-def demo_plot(results: pd.DataFrame, title: str, y_label="Accuracy"):
+def demo_plot(results: pd.DataFrame, title: str, y_label="Accuracy", limits=None):
     """Lineplot: x axis is the number of demonstrations and the y axis is the accuracy
        plot the graph in a professional way for academic paper
     """
+    results = results[results["num_demonstrations"] <= limits] if limits else results
     results_tokens = results.groupby(["num_demonstrations", "rationales"]).mean().reset_index()
     results_tokens['tokens'] = results_tokens['tokens'].apply(lambda x: int(np.round(x)))
     sns.set(style="darkgrid", font_scale=1.5)
@@ -91,7 +92,7 @@ model_path = "/scratch/ylu130/model/llama-2-7b/"
 data_path = "/scratch/ylu130/processed_data/commonsenseqa/out-domain"
 acc_results = compute_metric(result_path, model_path, data_path, "commonsenseqa", compute_mc_acc)
 
-demo_plot(acc_results, "Out-domain CommonsenseQA Accuracy")
+demo_plot(acc_results, "Out-domain CommonsenseQA Accuracy", limits=9)
 token_plot(acc_results, "Out-domain CommonsenseQA Accuracy")
 
 # Note: RuntimeError: probability tensor contains either `inf`, `nan` or element < 0 -> Probability distribution has been messed
