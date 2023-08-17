a694c2c780b5b8eba3cd1a52dc2078c7679f091b
yining610 <kinglu20000216@gmail.com>
2023-08-16 22:22:35 -0400

    update

--- evaluate.py
+++ evaluate.py
@@ -16,26 +16,30 @@ def get_folders(path):
     return folders
 
 
-
 def compute_metric(result_path: str, model_path: str, data_path: str, data_name: str, metric_fn: callable):
     folders = get_folders(result_path)
 
-    results = {"num_demonstrations": [], "seed": [], "rationales": [], "tokens":[], "acc": []}
+    results = {"num_demonstrations": [], "seed": [], "rationales": [], "tokens":[], "acc": [], "max_prompt_len": []}
     
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     for folder in folders:
         results["num_demonstrations"].append(int(folder.split("-")[0][1:]))
-        results["seed"].append(int(folder.split("-")[-2][1:]))
+        results["seed"].append(int(folder.split("-s")[-1].split("-")[0]))
         if "True" in folder:
             results["rationales"].append("With Rationales")
         else:
             results["rationales"].append("Without Rationales")
         results['acc'].append(metric_fn(os.path.join(result_path, folder)))
 
-        with open(os.path.join(data_path, folder, f"{data_name}.jsonl"), "r") as f:
+        if "-m" in folder:
+            results["max_prompt_len"].append(int(folder.split("-m")[-1]))
+        else:
+            results["max_prompt_len"].append(2048) # default max prompt length
+
+        with open(os.path.join(data_path, folder.split("-m")[0], f"{data_name}.jsonl"), "r") as f:
             data = f.readlines()[0]
         demonstrations = json.loads(data)['prompt']
-        demonstrations = demonstrations.split("### Demonstration:")[1].split("### Input:")[0].strip()
+        # demonstrations = demonstrations.split("### Demonstration:")[1].split("### Input:")[0].strip()
         demonstrations_ids = tokenizer(demonstrations, return_tensors="pt")
         results["tokens"].append(demonstrations_ids["input_ids"].shape[1])
 
@@ -52,13 +56,22 @@ def demo_plot(results: pd.DataFrame, title: str, y_label="Accuracy", limits=None
     results_tokens = results.groupby(["num_demonstrations", "rationales"]).mean().reset_index()
     results_tokens['tokens'] = results_tokens['tokens'].apply(lambda x: int(np.round(x)))
     sns.set(style="darkgrid", font_scale=1.5)
-    plt.figure(figsize=(12, 6))
-    ax = sns.lineplot(x="num_demonstrations", y="acc", 
-                      hue="rationales", style="rationales",
-                      data=results, markers=True, dashes=False, 
-                      markersize=10
-                      )
-    # label each point with the number of tokens\
+    plt.figure(figsize=(14, 6))
+
+    if len(results['max_prompt_len'].unique()) == 1:
+        ax = sns.lineplot(x="num_demonstrations", y="acc", 
+                          hue="rationales", style="rationales",
+                          data=results, markers=True, dashes=False, 
+                          markersize=10
+                          )
+    else:
+        ax = sns.lineplot(x="num_demonstrations", y="acc", 
+                          hue="rationales", style="max_prompt_len",
+                          data=results, markers=True, dashes=False,
+                          markersize=10
+                         )
+
+    # label each point with the number of tokens
     for i in range(len(results_tokens)):
         ax.text(results_tokens.iloc[i]['num_demonstrations'], 
                 results_tokens.iloc[i]['acc'], 
@@ -77,12 +90,19 @@ def token_plot(results: pd.DataFrame, title: str, y_label="Accuracy"):
     draw smoothed line plot for each rationales: x axis is the number of tokens in the demonstration and the y axis is the accuracy
     """
     sns.set(style="darkgrid", font_scale=1.5)
-    plt.figure(figsize=(12, 6))
-    ax = sns.lineplot(x="tokens", y="acc", 
-                      hue="rationales", style="rationales",
-                      data=results, markers=True, dashes=False, 
-                      markersize=10
-                      )
+    plt.figure(figsize=(14, 6))
+    if len(results['max_prompt_len'].unique()) == 1:
+        ax = sns.lineplot(x="tokens", y="acc", 
+                          hue="rationales", style="rationales",
+                          data=results, markers=True, dashes=False, 
+                          markersize=10
+                          )
+    else:
+        ax = sns.lineplot(x="tokens", y="acc", 
+                          hue="rationales", style="max_prompt_len",
+                          data=results, markers=True, dashes=False,
+                          markersize=10
+                         )
     ax.set_title(title)
     ax.set_ylabel(y_label)
     plt.show()
@@ -110,8 +130,12 @@ model_path = "/scratch/ylu130/model/llama-2-7b/"
 data_path = "/scratch/ylu130/processed_data/gsm8k/out-domain"
 acc_results = compute_metric(result_path, model_path, data_path, "gsm8k", compute_mc_acc)
 
+demo_plot(acc_results[acc_results['max_prompt_len'] == 2048], "Out-domain GSM8K Accuracy", limits=9)
+token_plot(acc_results[acc_results['max_prompt_len'] == 2048], "Out-domain GSM8K Accuracy")
+
 demo_plot(acc_results, "Out-domain GSM8K Accuracy", limits=9)
 token_plot(acc_results, "Out-domain GSM8K Accuracy")
+
 
 
 # Note: RuntimeError: probability tensor contains either `inf`, `nan` or element < 0 -> Probability distribution has been messed
--- metric.py
+++ metric.py
@@ -83,49 +83,6 @@ def compute_grouped_metrics(predictions, references, groups, xlingual=False):
     return results
 
 def match_multiplechoice_answer(answer, text):
-    # if text is None:
-    #     return False
-    # answer_label = answer.split(":")[0].strip().lower()
-    # if answer_label == "choice1":
-    #     answer_label = "a"
-    # elif answer_label == "choice2":
-    #     answer_label = "b"
-    # elif answer_label == "choice3":
-    #     answer_label = "c"
-    # elif answer_label == "choice4":
-    #     answer_label = "d"
-    # elif answer_label == "choice5":
-    #     answer_label = "e"
-    # answer_text = answer.split(":")[1].strip().lower().replace(" ", "")
-
-    # texts = text.split(":")
-    # text_label = texts[0].strip().lower().split(" ")[-1].strip().lower()
-    # if text_label == "choice1":
-    #     text_label = "a"
-    # elif text_label == "choice2":
-    #     text_label = "b"
-    # elif text_label == "choice3":
-    #     text_label = "c"
-    # elif text_label == "choice4":
-    #     text_label = "d"
-    # elif text_label == "choice5":
-    #     text_label = "e"
-    # elif text_label == "1":
-    #     text_label = "a"
-    # elif text_label == "2":
-    #     text_label = "b"
-    # elif text_label == "3":
-    #     text_label = "c"
-    # elif text_label == "4":
-    #     text_label = "d"
-    # elif text_label == "5":
-    #     text_label = "e"
-    
-    # if answer_label == text_label:
-    #     return True
-    
-    # if answer_text in text.strip().lower().replace(" ", ""):
-    #     return True
     answer = answer.strip().lower()
     text = text.strip().lower()
     if answer in text or answer.replace(" ", "") in text:
