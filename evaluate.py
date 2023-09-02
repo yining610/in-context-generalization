10ee272cea6cac389d8ff781926d927430a62d2e
yining610 <kinglu20000216@gmail.com>
2023-09-02 13:42:03 -0400

    update

--- evaluate.py
+++ evaluate.py
@@ -109,8 +109,6 @@ def token_plot(results: pd.DataFrame, title: str, y_label="Accuracy"):
     ax.set_ylabel(y_label)
     plt.show()
 
-# TODO: ADD 0 demonstration results to without rationales
-
 base_path = "./results/llama2-7b"
 base_path_2 = "/scratch/ylu130/processed_data"
 model_path = "meta-llama/Llama-2-7b-hf"
@@ -130,12 +128,18 @@ acc_results2 = get_results(result_path=os.path.join(base_path, "commonsenseqa/in
                            data_path=os.path.join(base_path_2, "commonsenseqa/in-domain"), 
                            data_name="commonsenseqa", 
                            metric_fn=compute_mc_acc)
+
 demo_plot(acc_results2, "In-domain Commonsenseqa Accuracy", limits=9)
 token_plot(acc_results2, "In-domain Commonsenseqa Accuracy")
 
 # CommonsenseQA overall results
 acc_results1['num_demonstrations'] = acc_results1['num_demonstrations'].apply(lambda x: -x)
 acc_results = pd.concat([acc_results1, acc_results2], ignore_index=True)
+
+zero_demo_temp = acc_results[acc_results['num_demonstrations'] == 0].copy()
+zero_demo_temp['rationales'] = "Without Rationales"
+acc_results = pd.concat([acc_results, zero_demo_temp], ignore_index=True)
+
 demo_plot(acc_results, "Commonsenseqa Overall Accuracy", limits=9, show_tokens=False)
 
 # GSM8K out-domain results
@@ -145,23 +149,8 @@ acc_results3 = get_results(result_path=os.path.join(base_path, "gsm8k/out-domain
                             data_name="gsm8k",
                             metric_fn=compute_math_acc)
 
-demo_plot(acc_results3[acc_results3['max_prompt_len'] == 2048], "Out-domain GSM8K Accuracy", limits=9)
-token_plot(acc_results3[acc_results3['max_prompt_len'] == 2048], "Out-domain GSM8K Accuracy")
+# demo_plot(acc_results3[acc_results3['max_prompt_len'] == 2048], "Out-domain GSM8K Accuracy", limits=9)
+# token_plot(acc_results3[acc_results3['max_prompt_len'] == 2048], "Out-domain GSM8K Accuracy")
 
 demo_plot(acc_results3, "Out-domain GSM8K Accuracy", limits=9)
 token_plot(acc_results3, "Out-domain GSM8K Accuracy")
-
-
-# Note: RuntimeError: probability tensor contains either `inf`, `nan` or element < 0 -> Probability distribution has been messed
-
-base_path = "./results/opt-1.3b"
-base_path_2 = "/scratch/ylu130/processed_data"
-model_path = "/scratch/ylu130/model/opt-1.3b/"
-# Commonsense out-domain results
-acc_results4 = get_results(result_path=os.path.join(base_path, "commonsenseqa/out-domain"),
-                            model_path=model_path,
-                            data_path=os.path.join(base_path_2, "commonsenseqa/out-domain"),
-                            data_name="commonsenseqa",
-                            metric_fn=compute_mc_acc)
-demo_plot(acc_results4, "Out-domain Commonsense Accuracy", limits=9)
-token_plot(acc_results4, "Out-domain Commonsense Accuracy")
